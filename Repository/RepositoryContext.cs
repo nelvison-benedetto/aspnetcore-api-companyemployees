d81c219ab40cb0e96cfdea6d0c004eba7f743552
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-07 23:31:17 +0100

    update

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -7,19 +7,20 @@ namespace CompanyEmployees.Repository
 {
     public class RepositoryContext : DbContext
     {
-        //DbContext è il bridge code c# - Db. ogni DbContext è un 'contenitore' di tabs-view-queries, gestisce il tracking delle entities, genera sql, gestisce le config del modello (pk, relazioni, vincoli, lunghezze campi, ect)
+        //DbContext è il bridge code c# - Db. DbContext rappresenta una sessione con il database, tiene traccia delle entity (Change Tracker), coordina le query e le operazioni CRUD, gestisce le config del modello (pk, relazioni, vincoli, lunghezze campi, ect), usa il modello EF per tradurre LINQ in SQL
 
         public RepositoryContext(DbContextOptions options) : base(options) {  
         }
-        //DbContextOptions contiene configurazioni (tipo database, stringa connessione). inietti here w DI, base(options) chiam il constrct di DbContextOptions
-        
+        //DbContextOptions contiene le configurazioni del DbContext (provider, connection string, ecc.), viene iniettato tramite DI e passato al costruttore base di DbContext
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CompanyConfiguration());  //SEE Configuration/CompanyConfiguration.cs x info details!!!
             modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
         }
-        //add(inietta w ApplyConfiguration()) regole custom (Fluent API)(e.g.pk composte,lenght strs,relations,vincoli,ect) salvate in un file (e.g.Configuration/CompanyConfiguration.cs) quando EF costruisce il modello della entity
+        //OnModelCreating viene eseguito durante la fase di costruzione del modello EF, ApplyConfiguration applica le configurazioni Fluent API definite separatamente (chiavi, relazioni, vincoli, lunghezze campi, ecc.) attualmente nei files configurations/CompanyConfiguration.cs  configurations/EmployeeConfiguration.cs
 
+        //salvate 
         public DbSet<Company> Companies { get; set; }
         public DbSet<Employee> Employees { get; set; }
         //DbSet<T> è tabella del db, e.g. EF capisce che Companies -> tab db Companies.
